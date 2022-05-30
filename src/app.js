8a0a62a987209c110161ee8e72af91ace3ee5087
mendoza099 <sergiomendozadominguez@gmail.com>
2022-05-30 10:12:18 +0000

    name-generator

--- src/app.js
+++ src/app.js
@@ -9,3 +9,30 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let domain = [".com", ".es"];
+var anyString = [];
+
+function combinaciones(initial, middle, end, dom) {
+  for (i = 0; i < initial.length; i++) {
+    var param1 = initial[i];
+
+    for (k = 0; k < middle.length; k++) {
+      var param2 = middle[k];
+
+      for (j = 0; j < end.length; j++) {
+        var param3 = end[j];
+
+        for (l = 0; l < domain.length; l++) {
+          var param4 = dom[l];
+          var total = anyString.push(param1 + param2 + param3 + param4);
+        }
+      }
+    }
+  }
+  return anyString;
+}
+console.log(combinaciones(pronoun, adj, noun, domain));
